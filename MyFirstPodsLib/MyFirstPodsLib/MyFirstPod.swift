440c0b9677bd7d42c84fecd0ee8fc3a44af9e953
Mahesh Dhapa <mahesh@cognitionteam.com>
2022-02-08 14:57:07 +0530

    Created PodSpec

---
+++ MyFirstPodsLib/MyFirstPodsLib/MyFirstPod.swift
@@ -0,0 +1,23 @@
+//
+//  MyFirstPod.swift
+//  MyFirstPodsLib
+//
+//  Created by Cognition on 08/02/22.
+//
+
+import Foundation
+
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
---
+++ MyFirstPodsLib/MyFirstPodsLib/MyFirstPodsLib.h
@@ -0,0 +1,18 @@
+//
+//  MyFirstPodsLib.h
+//  MyFirstPodsLib
+//
+//  Created by Cognition on 08/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstPodsLib.
+FOUNDATION_EXPORT double MyFirstPodsLibVersionNumber;
+
+//! Project version string for MyFirstPodsLib.
+FOUNDATION_EXPORT const unsigned char MyFirstPodsLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstPodsLib/PublicHeader.h>
+
+
